5d0d5d641f26963869d1242701beee356367bad8
onyxdragun <tyler@dynamicshark.com>
2024-08-21 13:40:11 -0700

    Added route for serving user avatar

--- src/routers/user.js
+++ src/routers/user.js
@@ -148,4 +148,21 @@ router.delete('/users/me', auth, async (req, res) => {
     }
 });
 
+//
+// Fetch user avatar
+// ** Currently stored as Base64 in the DB
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user || !user.avatar) {
+            throw new Error();
+        }
+
+        res.set('Content-Type', 'image/jpg');
+        res.send(user.avatar);
+    } catch(error) {
+        res.status(404).send();
+    }
+});
+
 export {router}
